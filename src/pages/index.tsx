d270b9ff4e4a9391cc93fb0610d4006b58748f7d
Hee-Ku Yoon <heegu0311@gmail.com>
2023-02-19 13:48:04 +0900

    feat: 4.9 Responsive modifier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,142 +1,85 @@
 export default function Home() {
   return (
-    <>
-      <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-        <div className="bg-white p-6 rounded-3xl shadow-xl">
-          <span className="font-semibold text-3xl">Select Item</span>
-          <ul>
-            {[1, 2, 3, 4].map((i) => (
-              <div
-                key={i}
-                className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50"
-              >
-                <span className="text-gray-500">Grey Chair</span>
-                <span className="font-semibold">$19</span>
-              </div>
-            ))}
-          </ul>
-          <div className="flex justify-between mt-2 pt-2 border-t-4 border-dashed">
-            <span>Total</span>
-            <span className="font-semibold">$10</span>
-          </div>
-          <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
-            Checkout
-          </button>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
+      <div className="bg-white sm:bg-red-400 sm:hover:bg-red-800 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <ul>
+          {[1, 2, 3, 4].map((i) => (
+            <div
+              key={i}
+              className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50"
+            >
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        <div className="flex justify-between mt-2 pt-2 border-t-4 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
         </div>
-        <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-          <div className="bg-blue-500 p-6 pb-14">
-            <span className="text-white text-2xl">Profile</span>
+        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+          Checkout
+        </button>
+      </div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-zinc-500 rounded-full group-hover:bg-red-300 transition-colors" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
           </div>
-          <div className="rounded-3xl p-6 bg-white relative -top-5">
-            <div className="flex relative -top-16 items-end justify-between">
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-500">Orders</span>
-                <span className="font-medium">340</span>
-              </div>
-              <div className="h-24 w-24 bg-zinc-500 rounded-full group-hover:bg-red-300 transition-colors" />
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-500">Spent</span>
-                <span className="font-medium">$340</span>
-              </div>
-            </div>
-            <div className="relative  flex flex-col items-center -mt-10 -mb-5">
-              <span className="text-lg font-medium">Tony Molloy</span>
-              <span className="text-sm text-gray-500">미국</span>
-            </div>
+          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
           </div>
         </div>
-        <div className="bg-white p-10 rounded-3xl shadow-xl">
-          <div className="flex mb-5 justify-between items-center">
-            <span>➡️</span>
-            <div className="space-x-3">
-              <span>⭐4.9</span>️
-              <span className="shadow-xl p-2 rounded-md">💕</span>️
-            </div>
+      </div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>➡️</span>
+          <div className="space-x-3">
+            <span>⭐4.9</span>️
+            <span className="shadow-xl p-2 rounded-md">💕</span>️
           </div>
-          <div className="bg-zinc-400 h-72" />
-          <div className="flex flex-col">
-            <span className="font-medium text-xl">Swoon Lounge</span>
-            <span className="text-xs text-gray-500">Chair</span>
-            <div className="mt-3 mb-5 flex justify-between items-center">
-              <div className="space-x-2">
-                <button className="w-5 h-5 rounded-full bg-yellow-500  focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
-                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
-                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
-              </div>
-              <div className="flex items-center space-x-5">
-                <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
-                  -
-                </button>
-                <span>1</span>
-                <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
-                  +
-                </button>
-              </div>
+        </div>
+        <div className="bg-zinc-400 h-72" />
+        <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500  focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
             </div>
-            <div className="flex justify-between items-center">
-              <span className="font-medium text-2xl">$450</span>
-              <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-xl">
-                Add to chart
+            <div className="flex items-center space-x-5">
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                +
               </button>
             </div>
           </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-xl">
+              Add to chart
+            </button>
+          </div>
         </div>
-        <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
-        <form className="flex flex-col space-y-2 bg-blue-500 p-4">
-          <input
-            type="text"
-            required
-            placeholder="Username"
-            className="required:bg-green-100 invalid:text-red-300"
-            minLength={5}
-          />
-          <input
-            type="text"
-            disabled
-            placeholder="Username (Disabled)"
-            className="disabled:bg-red-300"
-            minLength={5}
-          />
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            className="placeholder:text-blue-300 peer"
-          />
-          <span className="hidden peer-invalid:block text-red-900">
-            Password is empty
-          </span>
-          <span className="hidden peer-valid:block text-yellow-500">
-            Password is filled
-          </span>
-
-          <input type="submit" />
-        </form>
       </div>
-      <div className="flex flex-col space-y-2 p-5">
-        <details className="select-none open:text-white open:bg-indigo-400">
-          <summary className="select-none cursor-pointer">
-            What is my fav. food?
-          </summary>
-          <span className=" selection:bg-blue-500 selection:text-white">
-            김치
-          </span>
-        </details>
-        <ul className="list-decimal marker:text-teal-500">
-          <li>Hi</li>
-          <li>Hi</li>
-          <li>Hi</li>
-        </ul>
-        <input
-          type={"file"}
-          className={
-            "file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border-2 file:transition-colors file:border-0 file:rounded-xl file:px-5  file:text-white file:bg-purple-400 file:border-2 file:border-transparent"
-          }
-        />
-        <p className="first-letter:text-7xl first-letter:hover:text-blue-400">
-          lorem asdmlkasdmvklasdmlkvasdlvnaskjvlkasdmlkasdcklmsalkc
-        </p>
-      </div>
-    </>
+    </div>
   );
 }
