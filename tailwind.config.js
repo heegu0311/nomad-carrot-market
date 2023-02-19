ed7738087824148e02edf5f7d2ac26b439d62ec0
Hee-Ku Yoon <heegu0311@gmail.com>
2023-02-19 17:41:50 +0900

    feat: 6.0 Fix compiler range issue of tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/libs/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
