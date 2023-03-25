daad370dcbf75f48be4df7f754d7cf9c96cce886
Hee-Ku Yoon <heegu0311@gmail.com>
2023-03-25 16:22:08 +0900

    7.4 validations

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,27 +1,49 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, watch, handleSubmit } = useForm<LoginForm>();
 
-  console.log(watch());
+  const onValid = (data: LoginForm) => {
+    console.log(data);
+
+    console.log("iam valid bby");
+  };
+
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "password is required" })}
         type="password"
         placeholder="Password"
-        required
       />
-      <input {...register("submit")} type="submit" value="Create Account" />
+      <input type="submit" value="Create Account" />
     </form>
   );
 }
