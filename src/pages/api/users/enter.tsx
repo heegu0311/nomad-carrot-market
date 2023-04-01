2f7a7a47552ca6e464f4751a712ee0b5b6d58993
Hee-Ku Yoon <heegu0311@gmail.com>
2023-04-01 13:15:53 +0900

    feat: 9.1 Account logic in api with if/else statement

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,8 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import { client } from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone,
+      },
+    });
+  }
+
+  if (user) console.log("found it");
+  else {
+    console.log("did not find. Will create.");
+    user = await client.user.create({
+      data: {
+        name: "Anonymous",
+        phone: +phone || Math.round(Math.random() * 1000000),
+        email,
+      },
+    });
+    console.log(`user with the email <${user.email}>is created`);
+  }
   return res.json({ ok: true });
 }
 
